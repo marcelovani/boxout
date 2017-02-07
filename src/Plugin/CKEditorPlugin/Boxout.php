a40d8ef145031246b5696566c363f818fc30bf7a
Marcelo Vani <marcellovani@yahoo.co.uk>
2017-02-07 22:01:51 +0000

    Preparing for drupal core 8.3

--- src/Plugin/CKEditorPlugin/Boxout.php
+++ src/Plugin/CKEditorPlugin/Boxout.php
@@ -1,15 +1,8 @@
 <?php
 
-/**
- * @file
- * Definition of \Drupal\boxout\Plugin\CKEditorPlugin\Boxout.
- */
-
 namespace Drupal\boxout\Plugin\CKEditorPlugin;
 
-use Drupal\ckeditor\CKEditorPluginInterface;
-use Drupal\ckeditor\CKEditorPluginButtonsInterface;
-use Drupal\Component\Plugin\PluginBase;
+use Drupal\ckeditor\CKEditorPluginBase;
 use Drupal\editor\Entity\Editor;
 
 /**
@@ -17,10 +10,11 @@ use Drupal\editor\Entity\Editor;
  *
  * @CKEditorPlugin(
  *   id = "boxout",
- *   label = @Translation("Boxout")
+ *   label = @Translation("Boxout"),
+ *   module = "boxout"
  * )
  */
-class Boxout extends PluginBase implements CKEditorPluginInterface, CKEditorPluginButtonsInterface {
+class Boxout extends CKEditorPluginBase {
   /**
    * Gets a path to module.
    *
@@ -64,7 +58,7 @@ class Boxout extends PluginBase implements CKEditorPluginInterface, CKEditorPlug
    */
   public function getConfig(Editor $editor) {
     return array(
-      'boxout_dialog_title_insert' => t('Insert Boxout'),
+      'boxout_dialog_title_insert' => $this->t('Insert Boxout'),
     );
   }
 
@@ -74,7 +68,7 @@ class Boxout extends PluginBase implements CKEditorPluginInterface, CKEditorPlug
   public function getButtons() {
     return array(
       'Boxout' => array(
-        'label' => t('Boxout'),
+        'label' => $this->t('Boxout'),
         'image' => $this->path() . '/js/plugins/boxout/boxout.png',
       ),
     );
